593a6fde3ded2e4f33318e28ede3e6b814301387
BlaiseCorcoran <132222583+BlaiseCorcoran@users.noreply.github.com>
2024-04-13 14:45:34 -0400

    Log in should be working

--- PGFApp/app/src/main/java/com/example/pgfapp/MainActivity.kt
+++ PGFApp/app/src/main/java/com/example/pgfapp/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.pgfapp
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+
+    lateinit var usrnameInput: EditText
+    lateinit var pwdInput: EditText
+    lateinit var loginButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +24,17 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        usrnameInput = findViewById(R.id.username_input)
+        pwdInput = findViewById(R.id.password_input)
+        loginButton = findViewById(R.id.login_button)
+
+        loginButton.setOnClickListener{
+            val username = usrnameInput.text.toString()
+            val pwd = pwdInput.text.toString()
+
+            Log.i("Test Credents","Username: $username and Password: $pwd")
         }
     }
 }
