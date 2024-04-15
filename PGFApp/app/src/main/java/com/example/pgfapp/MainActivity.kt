e21b228f9ea557947ead9d2d734c635263e5f40e
BlaiseCorcoran <132222583+BlaiseCorcoran@users.noreply.github.com>
2024-04-15 14:44:19 -0400

    Firebase integration and updated login page that accepts email instead of a username

--- PGFApp/app/src/main/java/com/example/pgfapp/MainActivity.kt
+++ PGFApp/app/src/main/java/com/example/pgfapp/MainActivity.kt
@@ -8,13 +8,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
 
-    private lateinit var usrnameInput: EditText
+    private lateinit var emailInput: EditText
     private lateinit var pwdInput: EditText
     private lateinit var loginButton: Button
+    private lateinit var auth: FirebaseAuth
+    //init FBA
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,15 +33,27 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        usrnameInput = findViewById(R.id.username_input)
+        emailInput = findViewById(R.id.username_input)
         pwdInput = findViewById(R.id.password_input)
         loginButton = findViewById(R.id.login_button)
+        auth = Firebase.auth
 
         loginButton.setOnClickListener{
-            val username = usrnameInput.text.toString()
+            val email = emailInput.text.toString()
             val pwd = pwdInput.text.toString()
 
-            Log.i("Test Credents","Username: $username and Password: $pwd")
+            Log.i("Test Credents","Username: $email and Password: $pwd")
         }
     }
+
+    public override fun onStart(){
+        super.onStart()
+        //checks to see if the user is signed in
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            recreate()
+        }
+    }
+
+
 }
