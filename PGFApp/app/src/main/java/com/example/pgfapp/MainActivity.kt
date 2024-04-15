46fe986fcef6baf245114c733edf73bf1a0c6aff
BlaiseCorcoran <132222583+BlaiseCorcoran@users.noreply.github.com>
2024-04-15 16:26:52 -0400

    added a next button (sends user to the maps page) and some temp auth login stuff (it just says "Authentication Failed")

--- PGFApp/app/src/main/java/com/example/pgfapp/MainActivity.kt
+++ PGFApp/app/src/main/java/com/example/pgfapp/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.pgfapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,6 +15,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
+
 class MainActivity : AppCompatActivity() {
 
 
@@ -19,7 +23,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var pwdInput: EditText
     private lateinit var loginButton: Button
     private lateinit var auth: FirebaseAuth
-    //init FBA
+    private lateinit var nextBtn: Button
 
 
 
@@ -42,7 +46,27 @@ class MainActivity : AppCompatActivity() {
             val email = emailInput.text.toString()
             val pwd = pwdInput.text.toString()
 
-            Log.i("Test Credents","Username: $email and Password: $pwd")
+            auth.signInWithEmailAndPassword(email, pwd)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("@id/email", "success")
+                        val user = auth.currentUser
+                        //updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("", "failure", task.exception)
+                        Toast.makeText(
+                            baseContext,
+                            "Authentication failed.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                        //updateUI(null)
+                    }
+                }
+
+            /* remove this for the final build */
+            Log.i("Test Credentials","Username: $email and Password: $pwd")
         }
     }
 
@@ -50,10 +74,15 @@ class MainActivity : AppCompatActivity() {
         super.onStart()
         //checks to see if the user is signed in
         val currentUser = auth.currentUser
-        if(currentUser != null){
+        if(currentUser != null) {
             recreate()
         }
+
     }
 
+    //Sends the user to the maps activity
+    fun gotoMaps(v: View?) {
+        startActivity(Intent(this@MainActivity, MapsActivity::class.java))
+    }
 
 }
