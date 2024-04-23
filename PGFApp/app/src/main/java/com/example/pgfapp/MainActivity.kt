84f3f46062c88396fe43dea6dcba6cf772111a8d
BlaiseCorcoran <132222583+BlaiseCorcoran@users.noreply.github.com>
2024-04-22 23:22:37 -0400

    Temp buttons removed, sign in sends user to the maps page, sign in page uses font from figma file, and documentation added to the sign in page

--- PGFApp/app/src/main/java/com/example/pgfapp/MainActivity.kt
+++ PGFApp/app/src/main/java/com/example/pgfapp/MainActivity.kt
@@ -18,18 +18,24 @@ import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
+    //PURPOSE:  Allow the user to sign in and/or sign up.
+    //          If the sign in is successful,
+    //              ->the user is taken to the maps page
+    //          If the sign in is unsuccessful,
+    //              ->the user is shown an error message
+    //          When the user taps the sign up button,
+    //              ->They are taken to an account creation page
 
-    private lateinit var emailInput: EditText
-    private lateinit var pwdInput: EditText
-    private lateinit var loginButton: Button
-    private lateinit var signedOut: Button
-    private lateinit var auth: FirebaseAuth
+    //Global variables
+    private lateinit var emailInput: EditText //email input field
+    private lateinit var pwdInput: EditText //password input field
+    private lateinit var loginButton: Button //login button
+    private lateinit var auth: FirebaseAuth //firebase authentication
 
-
-
-    /*TO DO:
-    * MAKE SURE TO INCLUDE A SIGN OUT BUTTON ONCE THE SIGN IN FUNCTIONALITY
-    * HAS BEEN COMPLETED
+    /*
+    Function: OnCreate
+    Parameter: savedInstanceState: Bundle? [value]
+    Description: This function sets up the instance of this page.
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,12 +47,12 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        emailInput = findViewById(R.id.email_input)
-        pwdInput = findViewById(R.id.password_input)
-        loginButton = findViewById(R.id.login_button)
-        signedOut = findViewById(R.id.signout_button)
-        auth = Firebase.auth
+        emailInput = findViewById(R.id.email_input) //email input
+        pwdInput = findViewById(R.id.password_input) //password input
+        loginButton = findViewById(R.id.login_button) //login button
+        auth = Firebase.auth //firebase authenticator
 
+        //If the user taps the sign in button
         loginButton.setOnClickListener{
             val email = emailInput.text.toString()
             val pwd = pwdInput.text.toString()
@@ -54,15 +60,16 @@ class MainActivity : AppCompatActivity() {
             auth.signInWithEmailAndPassword(email, pwd)
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
-                        // Sign in success, update UI with the signed-in user's information
+                        // Sign in is successful
                         Log.d("@id/email" + "@id/pwd", "success")
                         val user = auth.currentUser
+                        gotoMaps() //sends the user to the maps page
                     } else {
                         // If sign in fails, display a message to the user.
                         Log.w("", "failure", task.exception)
                         Toast.makeText(
                             baseContext,
-                            "Authentication failed.",
+                            "Sign-in failed. Please Try Again",
                             Toast.LENGTH_SHORT,
                         ).show()
                     }
@@ -71,13 +78,13 @@ class MainActivity : AppCompatActivity() {
             /* remove this for the final build */
             Log.i("Test Credentials","Username: $email and Password: $pwd")
         }
-
-        signedOut.setOnClickListener{
-            Firebase.auth.signOut()
-        }
     }
 
-    //checks if a log-in session is active
+    /*
+    Function Name: onStart
+    Parameters: None
+    Description: checks if a log-in session is active
+     */
     public override fun onStart(){
         super.onStart()
         //checks to see if the user is signed in
@@ -85,11 +92,20 @@ class MainActivity : AppCompatActivity() {
         //if(currentUser != null) {
             //recreate() //recreate() RECREATES the instance of this page
         //}
+        // /\
+        // |
+        // |
+        // Not sure if this should be removed or not
+        // I'll just keep it like this for now
 
     }
 
-    //Sends the user to the maps activity
-    fun gotoMaps(v: View?) {
+    /*
+    Function Name: gotoMaps
+    Parameters: None
+    Description: Sends the user to the maps page
+     */
+    fun gotoMaps() {
         startActivity(Intent(this@MainActivity, MapsActivity::class.java))
     }
 
