9af0b564b55732f6b551f54b34ea89eb229e6d59
dkram150 <151053698+dkram13@users.noreply.github.com>
2024-10-18 10:59:46 -0400

    Update MapsActivity.kt

--- PGFApp MOST UP TO DATE/app/src/main/java/com/example/pgfapp/MapsActivity.kt
+++ PGFApp MOST UP TO DATE/app/src/main/java/com/example/pgfapp/MapsActivity.kt
@@ -16,7 +16,7 @@ import com.google.android.gms.maps.model.Polygon
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import org.eclipse.californium.core.CoapClient
+//import org.eclipse.californium.core.CoapClient
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -106,29 +106,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     Description: Sends the user to the draw boundaries activity
     */
     fun gotoDrawBounds(v: View?){
-       /* val db = Firebase.firestore
-        val user = Firebase.auth.currentUser
-        if (user != null) {
-            val uid = user?.uid
-
-            val usertg = hashMapOf(
-                "first" to uid,
-                "last" to "this is test 2 of getting uid",
-                "born" to 1815
-            )
-            db.collection("userstg")
-                .add(usertg)
-                .addOnSuccessListener { documentReference ->
-                    Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
-                }
-                .addOnFailureListener { e ->
-                    Log.w(TAG, "Error adding document", e)
-                }// User is signed in
-        } else {
-            // No user is signed in
-        }
-*/
         startActivity(Intent(this@MapsActivity, BoundsActivity::class.java))
     }
+
 
 }
