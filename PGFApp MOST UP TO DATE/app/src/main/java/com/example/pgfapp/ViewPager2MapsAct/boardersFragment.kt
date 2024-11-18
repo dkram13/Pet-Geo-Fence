51923f019784755f086be30198e69ded7d48d06f
dkram150 <151053698+dkram13@users.noreply.github.com>
2024-11-18 12:25:54 -0500

    Merge branch 'main' of https://github.com/dkram13/Pet-Geo-Fence

--- PGFApp MOST UP TO DATE/app/src/main/java/com/example/pgfapp/ViewPager2MapsAct/boardersFragment.kt
+++ PGFApp MOST UP TO DATE/app/src/main/java/com/example/pgfapp/ViewPager2MapsAct/boardersFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.pgfapp.DatabaseStuff.DatabaseViewModel
+import com.example.pgfapp.R
 
 
 class boardersFragment : Fragment() {
@@ -22,8 +23,8 @@ class boardersFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        /*val view = inflater.inflate(R.layout.fragment_boarders, container, false)
-
+        val view = inflater.inflate(R.layout.fragment_boarders, container, false)
+/*
         // Find the container where buttons will be dynamically added
         val buttonContainer = view.findViewById<LinearLayout>(R.id.button_container)
 
